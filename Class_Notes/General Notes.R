40d04e06e6f93781305cddab145ad1e8de92e6b3
Lauren Guerrero Silva <laurenguerrerosilva@laurens-air.ad.uvu.edu>
2023-01-18 14:27:55 -0700

    trying

---
+++ Class_Notes/General Notes.R
@@ -0,0 +1,19 @@
+# Class notes Jan 17
+
+# How to upload files to GitHub
+# 1. Make any change 
+#-New file
+#-delete file
+#-Change file
+# 2. git add _____ name of file
+# 3. git commit -m " " 
+# 4. git push
+
+
+# Updating from others
+# 1. Navigate to folder
+# 2. git pull
+
+#:)
+
+# cammand + option + b  runs code before cursor
---
+++ Class_Notes/Week 2 in class.R
@@ -0,0 +1,56 @@
+# Week 2
+getwd()
+list.files(recursive = TRUE)
+list.files(path = "Data",
+           pattern = ".csv",
+           full.names = TRUE)
+
+#Give me the full path to wingspan file
+list.files(recursive = TRUE,
+           pattern = 'wingspan',
+           ignore.case = TRUE,
+           full.names = TRUE)
+# Option + - <-  assing name to code
+path <- list.files(recursive = TRUE,
+                   pattern = 'wingspan_vs',
+                   ignore.case = TRUE,
+                   full.names = TRUE)
+
+# csv comma separated value
+# Hightlight and command + return - only runs that part
+wingspan <- read.csv(path)
+
+# command + option + b - runs code before cursor
+
+#continue
+
+m <- wingspan$mass
+# $ name of variables.
+v <- wingspan$velocity
+s <- wingspan$variety
+
+# data frame - 2 dimensional data: rows and columns. 
+# vector - 1 dimensional (kinda like a list)
+
+# max() min() mean() - maximum, minumum, average
+
+max(m)
+min(m)
+mean(m)
+summary(m)
+cumsum(m) #cumulative sum
+plot(m)
+plot(cumsum(m))
+plot(sort(m))
+
+# x & y axis
+
+plot(x=wingspan$wingspan,
+     y=wingspan$mass)
+# cor - correlation 
+
+cor(x=wingspan$wingspan,
+    y=wingspan$mass)
+
+
+
