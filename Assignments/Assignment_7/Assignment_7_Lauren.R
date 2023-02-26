b19a6634489df5a2be0774910638d50cf3c6de0a
Lauren Guerrero Silva <laurenguerrerosilva@dhcp-161-28-233-165.ad.uvu.edu>
2023-02-25 18:22:31 -0700

    ass 7 update

--- Assignments/Assignment_7/Assignment_7_Lauren.R
+++ Assignments/Assignment_7/Assignment_7_Lauren.R
@@ -19,9 +19,7 @@ clean_1 <- df %>%
   pivot_longer(cols = -c(county, pop_2010),
                names_to = "religion",
                values_to = "proportion")
-  
-
-
+# Graph
 clean_1 %>% 
   ggplot(aes(x=reorder(county,pop_2010),y=proportion,
                      color = religion)) +
@@ -33,22 +31,62 @@ clean_1 %>%
                                    vjust = .5)) +
   labs(x = "County", y = "Population Proportion")
 
-GGally::ggpairs(clean_1, cardinality_threshold = 30)
+# trying something
+#GGally::ggpairs(clean_1, cardinality_threshold = 30)
 
-# The doesn't seem to be any correlation. 
+# Finding correlation 
+religions <- clean_1$religion %>% unique
+
+cor = numeric()
+for( i in religions){
+  loop <- 
+    clean_1 %>% filter(religion == i)
+  cor[i] <- 
+    cor(x=loop$pop_2010 , y=loop$proportion)
+}
+cor_data <- data.frame(religion=names(cor), correlation = unname(cor)) %>% 
+  arrange(correlation)
 
 
-# cant figure out the correlation stuff.
+# The doesn't seem to be any meaningful correlation between population and religion proportion.
+# The biggest counties are the ones to show more muslim religiuos proportion. 
 
 # 2. Does proportion of any specific religion in a given county correlate 
 #with the proportion of non-religious people?
-clean_1 %>% 
+
+# New data frame that has the religious as an individual column. 
+
+df_2 <- read_csv("./Utah_Religions_by_County.csv") %>% 
+  clean_names()
+
+clean_2 <- df_2 %>%
+  pivot_longer(cols = -c(county, pop_2010,religious),
+               names_to = "religion",
+               values_to = "proportion")
+
+
+clean_2 %>% 
   ggplot(aes(x=proportion,y=religion, color = religion)) +
   facet_wrap(~county) +
-  geom_point() +
+  geom_point(alpha = 1) +
+  geom_smooth(se=TRUE, method = "lm") +
   theme(axis.text.x = element_text(size = 8,
                                    vjust = .5))
 
-# The seems to be a inverse corratelation between non religions people and LDS people. 
-#The more LDS people present in a county the lowe the non religious people 
+
+cor = numeric()
+for( i in religions){
+  loop <- 
+    clean_2 %>% filter(religion == i) %>% 
+    mutate(non_reg_prop = 1 - religious)
+  cor[i] <- 
+    cor(x=loop$proportion , y=loop$non_reg_prop)
+}
+non_reg_cor_data <- data.frame(religion=names(cor), correlation = unname(cor)) %>% 
+  arrange(correlation)
+
+
+# The seems to be a inverse correlation between non religions people and LDS people. 
+#The more LDS people present in a county the lower the non religious people 
+
 
